10432827703be21d14e5c5a0668a665b14c2a1a8
YUCI SHEN <36720898+yshen901@users.noreply.github.com>
2019-09-05 23:48:48 -0700

    Split each class into its own file

--- W3D5/map.rb
+++ W3D5/map.rb
@@ -1,39 +1,3 @@
-class Stack
-    def initialize
-        @stack = []
-    end
-
-    def push(el)
-        @stack.push(el)
-    end
-
-    def pop
-        @stack.pop
-    end
-
-    def peek
-        @stack[-1]
-    end
-end
-
-class Queue
-    def initialize
-        @queue = []
-    end
-
-    def enqueue(el)
-        @queue.push(el)
-    end
-
-    def dequeue
-        @queue.shift
-    end
-
-    def peek
-        @queue[0]
-    end
-end
-
 class Map
     def initialize
        @map = [] 
