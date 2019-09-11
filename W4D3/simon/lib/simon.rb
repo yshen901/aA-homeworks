c240c57971e0d398753feafffcb9a5fa700d0e29
YUCI SHEN <36720898+yshen901@users.noreply.github.com>
2019-09-10 22:50:36 -0700

    Completed simon game logic

--- W4D3/simon/lib/simon.rb
+++ W4D3/simon/lib/simon.rb
@@ -4,38 +4,63 @@ class Simon
   attr_accessor :sequence_length, :game_over, :seq
 
   def initialize
-
+    @sequence_length = 1
+    @game_over = false
+    @seq = []
   end
 
   def play
-
+    take_turn until game_over
+    game_over_message
+    reset_game
   end
 
   def take_turn
-
+    show_sequence
+    sleep(1)
+    system("clear")
+    require_sequence
+    unless game_over
+      round_success_message 
+      @sequence_length += 1
+    end
+    system("clear")
   end
 
   def show_sequence
-
+    add_random_color
+    puts @seq.join(", ")
   end
 
   def require_sequence
-
+    puts "Time to test your memory! Please enter the sequence one color at a time"
+    puts "-----------------------------------------------------------------------"
+    @seq.each do |color|
+      print "Enter a color: "
+      input = gets.chomp
+      if input != color
+        @game_over = true
+        return
+      end
+      print "\nCorrect! "
+    end
   end
 
   def add_random_color
-
+    @seq << COLORS.sample
   end
 
   def round_success_message
-
+    puts "Round Passed!"
   end
 
   def game_over_message
-
+    puts "Game Over!"
   end
 
   def reset_game
-
+    @sequence_length = 1
+    @game_over = false
+    @seq = []
   end
 end
